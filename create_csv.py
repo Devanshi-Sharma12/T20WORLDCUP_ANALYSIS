4329e67a03602fdd871fa7c3c15204379b994cf5
Devanshi-Sharma12 <devanshisharma.2018780@gmail.com>
2024-07-05 11:57:06 +0530

    Add files via upload

---
+++ create_csv.py
@@ -0,0 +1,17 @@
+
+data = [
+    "Team,Player,Runs,Strike Rate",
+    "Afghanistan,Rahmanullah Gurbaz,281,124.33",
+    "India,Rohit Sharma,257,156.7",
+    "Australia,Travis Head,255,158.38",
+    "South Africa,Quinton de Kock,243,140.46",
+    "Afghanistan,Ibrahim Zadran,231,107.44"
+]
+
+csv_file_path = 't20_world_cup_2024(2).csv'
+
+with open(csv_file_path, 'w') as file:
+    for line in data:
+        file.write(line + '\n')
+
+print(f"CSV file '{csv_file_path}' has been created.")
