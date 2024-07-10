d2d671f0d821eb544866351710a0d6f4bf180b61
Devanshi-Sharma12 <devanshisharma.2018780@gmail.com>
2024-07-10 12:53:45 +0530

    Delete readcsv.py

--- readcsv.py
+++
@@ -1,9 +0,0 @@
-csv_file_path = 't20_world_cup_2024.csv'
-with open(csv_file_path, 'r') as file:
-    contents = file.readlines()
-data = []
-for line in contents:
-    row = line.strip().split(',')
-    data.append(row)
-for row in data:
-    print(row)
